c021850ec0f4f8b6aa3a33e3384980061dd4ec57
StefanoPetrilli <stefanop_1999@hotmail.it>
2022-11-23 22:33:38 +0100

    Initial commit

---
+++ utils/readFromFile.h
@@ -0,0 +1,14 @@
+//
+// Created by Stefano on 11/23/2022.
+//
+
+#ifndef BIOLOOG_READFROMFILE_H
+#define BIOLOOG_READFROMFILE_H
+
+namespace readFromFile {
+
+    int readFromFile(int path);
+
+};
+
+#endif //BIOLOOG_READFROMFILE_H
