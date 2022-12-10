7844e6e875a7da2e48e0031146757beb4ba4adba
StefanoPetrilli <stefanop_1999@hotmail.it>
2022-12-10 16:57:43 +0100

    Added tests for StringToCommand and for CommandToString

---
+++ test/utils/cli/command.cc
@@ -0,0 +1,24 @@
+//
+// Created by Stefano on 12/2/2022.
+//
+#include <gtest/gtest.h>
+
+#include "../../../src/utils/cli/command.h"
+
+namespace command_test {
+TEST(CommandToString, CommandCountNucleotidesShouldReturnExpectedText) {
+  EXPECT_EQ(cli::CommandToString(cli::Command::CountNucleotidesCommand),
+            "count_nucleotides\tTakes a DNA string and returns"
+            "the counting of each nucleotides in the format {'A', 'C', 'T', 'G'}");
+}
+
+TEST(CommandToString, CommandInvalidCommandShouldThrowDomainExcpetion) {
+  EXPECT_THROW(cli::CommandToString(cli::Command::InvalidCommand), std::domain_error);
+}
+
+TEST(StringToCommand, StringShouldReturnExpectedCommand) {
+  EXPECT_EQ(cli::StringToCommand("count_nucleotides"), cli::Command::CountNucleotidesCommand);
+
+  EXPECT_EQ(cli::StringToCommand("unknown_command"), cli::Command::InvalidCommand);
+}
+}
