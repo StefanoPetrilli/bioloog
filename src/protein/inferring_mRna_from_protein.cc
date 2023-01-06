e95edb8d372e9cdd8d8be76e93db64b0ab8c81ff
StefanoPetrilli <stefanop_1999@hotmail.it>
2023-01-06 11:40:52 +0100

    Added ParallelInferMRna

--- src/protein/inferring_mRna_from_protein.cc
+++ src/protein/inferring_mRna_from_protein.cc
@@ -4,16 +4,44 @@
 
 #include "inferring_mRna_from_protein.h"
 
-namespace protein
-{
-unsigned int InferMRna(const std::string& protein_sequence){
+namespace protein {
+unsigned int InferMRna(std::string &protein_sequence) {
+
+  protein_sequence.push_back(kStopCodon);
+
+#ifdef _OPENMP
+  return ParallelInferMRna(protein_sequence);
+#else
+  return SequentialInferMRna(protein_sequence);
+#endif
+}
+
+unsigned int SequentialInferMRna(const std::string &protein_sequence) {
   unsigned int result = 1;
 
-  for (auto protein : protein_sequence + kStopCodon)
-    result = (result * GetPossibleTranslations(protein)) % 1000000;
+  for (const char &i : protein_sequence)
+    result = (result * GetPossibleTranslations(i)) % kModule;
 
   return result;
 }
+
+unsigned int ParallelInferMRna(const std::string &protein_sequence) {
+  long result = 1;
+
+#pragma omp parallel shared(result, protein_sequence, kModule) default(none)
+  {
+#pragma omp single
+    {
+#pragma omp declare reduction (productAndModule : long : omp_out =  (omp_out * omp_in) % 1000000) initializer (omp_priv=omp_orig)
+#pragma omp taskloop num_tasks(1) reduction(productAndModule:result) shared(protein_sequence, kModule) default(none)
+      for (size_t i = 0; i < protein_sequence.length(); i++)
+        result = (result * GetPossibleTranslations(protein_sequence.at(i))) % kModule;
+    }
+  }
+
+  return result;
+}
+
 unsigned int GetPossibleTranslations(const char &protein) {
   return protein::kProteinToPossibleTranslation.at(protein);
 }
